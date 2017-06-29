2d8025306f05ebe649aa25cd7f9dbef6973411db
AllenCoder <AllenCoder@126.com>
2017-06-29 16:52:51 +0800

    Securing a Web Application

---
+++ SecuringaWebApplication/src/main/java/hello/WebSecurityConfigApplication.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Allen on 2017/6/29.
+ */
+@SpringBootApplication
+public class WebSecurityConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSecurityConfigApplication.class, args);
+    }
+
+}
+/*  <artifactId>Securing-a-Web-Application</artifactId>
+*    artifactId 格式不正常导致无法编译中间不能有空格
+* */
