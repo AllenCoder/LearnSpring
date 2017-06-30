36af1578157bed38c88c29d0a15e8eb807bddaf9
AllenCoder <AllenCoder@126.com>
2017-06-30 19:02:14 +0800

    Integrating Data

---
+++ IntegratingData/src/main/java/hello/IntegratingApplication.java
@@ -0,0 +1,7 @@
+package hello;
+
+/**
+ * Created by Allen on 2017/6/30.
+ */
+public class IntegratingApplication {
+}
---
+++ IntegratingData/src/test/java/FlowTests.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Allen on 2017/6/30.
+ */
+public class FlowTests {
+}
