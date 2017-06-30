f9d508888a1c990a533b622c6744ee547d2a1965
AllenCoder <AllenCoder@126.com>
2017-06-30 19:02:44 +0800

    Integrating Data

--- IntegratingData/src/main/java/hello/IntegratingApplication.java
+++ IntegratingData/src/main/java/hello/IntegratingApplication.java
@@ -1,7 +1,22 @@
 package hello;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+
+import java.io.IOException;
+
 /**
  * Created by Allen on 2017/6/30.
  */
+@SpringBootApplication
+@ImportResource("/hello/integration.xml")
 public class IntegratingApplication {
+    public static void main(String[] args) throws IOException {
+        ConfigurableApplicationContext ctx = new SpringApplication(IntegratingApplication.class).run(args);
+        System.out.println("args = [" + args + "]");
+        System.in.read();
+        ctx.close();
+    }
 }
